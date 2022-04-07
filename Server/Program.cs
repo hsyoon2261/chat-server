9517803568224b8668be90ca9f4153c19e3a1407
Heesang Yoon <85086571+hsyoon2261@users.noreply.github.com>
2022-04-07 22:59:32 +0900

    project initial setting

---
+++ Server/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        //ChatServerOption세팅, 이걸 활용해서 port, max client number등 변경시에
+        //재빌드없이 command로 쉽게 처리 가능.
+        static ChatServerOption ParseCommandLine(string[] args)
+        {
+            var result = CommandLine.Parser.Default.ParseArguments<ChatServerOption>(args) as CommandLine.Parsed<ChatServerOption>;
+
+            if (result == null)
+            {
+                Console.WriteLine("Failed Command Line");
+                return null;
+            }
+
+            return result.Value;
+        }
+    }
+    
+
+}
