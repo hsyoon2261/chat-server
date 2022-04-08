cf43c4018282af884a895f82b04a10050c2cbc3e
Heesang Yoon <85086571+hsyoon2261@users.noreply.github.com>
2022-04-08 12:25:44 +0900

    init

---
+++ ChatServer_1/Server/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //server setting
+            var serverOption = ParseCommandLine(args);
+            if(serverOption == null)
+            {
+                //use default setting
+                return;
+            }
+
+           //Mainserver = socket 통신 진입 class
+            var serverApp = new MainServer();
+            serverApp.InitConfig(serverOption);
+
+            serverApp.CreateStartServer();
+
+            MainServer.MainLogger.Info("Press q to shut down the server");
+            
+            while (true)
+            {
+                //why?
+                System.Threading.Thread.Sleep(50);
+
+                if (Console.KeyAvailable)
+                {
+                    //서버 중단을 막기 위한.. 
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+                    if (key.KeyChar == 'q')
+                    {
+                        Console.WriteLine("Server Terminate ~~~");
+                        serverApp.StopServer();
+                        break;
+                    }
+                }
+                                
+            }
+
+        }
+        //ChatServerOption세팅, 이걸 활용해서 port, max client number등 변경시에
+        //재빌드없이 command로 쉽게 처리 가능.
+        static ChatServerOption ParseCommandLine(string[] args)
+        {
+            var result = CommandLine.Parser.Default.ParseArguments<ChatServerOption>(args) as CommandLine.Parsed<ChatServerOption>;
+
+            if (result == null)
+            {
+                Console.WriteLine("Failed Command Line");
+                return null;
+            }
+
+            return result.Value;
+        }
+    }
+    
+
+}
