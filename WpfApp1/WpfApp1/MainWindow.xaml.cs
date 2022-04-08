33d69f4dfcc3430946e7d376065005016d244cac
Heesang Yoon <85086571+hsyoon2261@users.noreply.github.com>
2022-04-08 23:26:42 +0900

    wpf migration

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        //가입
+        private void Button_Click_0(object sender, RoutedEventArgs e)
+        {
+            labelConnState.Content = string.Format("먼저 접속해 주십시오.");
+        }
+
+
+        //접속
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string address = textBoxIP.Text;
+            if (checkBoxLocalHostIP.IsChecked == true)
+            {
+                address = "127.0.0.1";
+            }
+
+            int port = Convert.ToInt16(textBoxPort.Text);
+            try
+            {
+                
+
+            }
+            catch (Exception e)
+            {
+                labelConnState.Content = string.Format("{0}. 서버에 접속 실패", e.ToString());
+            }
+
+        }
+
+        // 로그아웃
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+        }
+
+        // 로그인
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+        }
+
+        // 접속 끊기
+        void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+        }
+
+        // 방 입장
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+        }
+
+        // 방 나가기
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+        }
+
+        // 방 채팅
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+        }
+
+        //버튼을 추가하였습니다.
+        private void TextBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+           
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+          
+        }
+
+        // 모든 게임 방 게임 시작
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+         
+        }
+
+        // 모든 게임 방 게임 끝
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+          
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/TcpController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace WpfApp1
+{
+    public class TcpController
+    {
+        public Socket Sock = null;   
+        public string LatestErrorMsg;
+        
+
+        //소켓연결        
+        public bool Connect(string ip, int port)
+        {
+            //string host = Dns.GetHostName();
+            //IPHostEntry ipHost = Dns.GetHostEntry(host);
+            //IPAddress ipAddr = ipHost.AddressList[1];
+            try
+            {
+                IPAddress serverIP = IPAddress.Parse(ip);
+                int serverPort = port;
+
+                Sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                Sock.Connect(new IPEndPoint(serverIP, serverPort));
+                if (Sock.Connected == true)
+                {
+                    byte[] sendBuff = Encoding.UTF8.GetBytes("Hello World this is for test");
+                    int sendBytes = Sock.Send(sendBuff);
+                }
+
+
+                if (Sock == null || Sock.Connected == false)
+                {
+                    return false;
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                LatestErrorMsg = ex.Message;
+                return false;
+            }
+        }
+
+        public Tuple<int,byte[]> Receive()
+        {
+
+            try
+            {
+                byte[] ReadBuffer = new byte[2048];
+                var nRecv = Sock.Receive(ReadBuffer, 0, ReadBuffer.Length, SocketFlags.None);
+
+                if (nRecv == 0)
+                {
+                    return null;
+                }
+
+                return Tuple.Create(nRecv,ReadBuffer);
+            }
+            catch (SocketException se)
+            {
+                LatestErrorMsg = se.Message;
+            }
+
+            return null;
+        }
+
+        //스트림에 쓰기
+        public void Send(byte[] sendData)
+        {
+            try
+            {
+                if (Sock != null && Sock.Connected) //연결상태 유무 확인
+                {
+                    Sock.Send(sendData, 0, sendData.Length, SocketFlags.None);
+                }
+                else
+                {
+                    LatestErrorMsg = "먼저 채팅서버에 접속하세요!";
+                }
+            }
+            catch (SocketException se)
+            {
+                LatestErrorMsg = se.Message;
+            }
+        }
+
+        //소켓과 스트림 닫기
+        public void Close()
+        {
+            if (Sock != null && Sock.Connected)
+            {
+                //Sock.Shutdown(SocketShutdown.Both);
+                Sock.Close();
+            }
+        }
+
+        public bool IsConnected() { return (Sock != null && Sock.Connected) ? true : false; }
+    }
+
+
+}
